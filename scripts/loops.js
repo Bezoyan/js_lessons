67acc1d120463d53a4479ea2311fd066a567557d
Bezoyan <zoya1905bezoyan@gmail.com>
2021-02-06 13:06:08 +0400

    forEach, map in javaScript

--- scripts/loops.js
+++ scripts/loops.js
@@ -3,12 +3,12 @@ const array = [4, 5, 9, 8, 5];
 
 // Returs values
 for (let i of array) {
-  console.log(i);
+  //console.log(i);
 }
 
 // Returs keys
 for (let i in array) {
-  console.log(i);
+  //console.log(i);
 }
 
 const obj2 = {
@@ -17,5 +17,39 @@ const obj2 = {
 };
 
 for (let i in obj2) {
-  console.log(i);
+  //console.log(i);
 }
+
+// // forEach
+const prices = [4.5, 5.3, 9, 8, 5];
+const tax = 0.23;
+
+const taxedPrice = [];
+
+// for (let price of prices) {
+//   taxedPrice.push(price * (1 + tax));
+// }
+
+prices.forEach((price, idx, arr) => {
+  const priceObj = {
+    index: idx,
+    taxedPrice: price * tax,
+  };
+  taxedPrice.push(priceObj);
+});
+
+console.log(taxedPrice);
+
+// map
+const prices = [4.5, 5.3, 9, 8, 5];
+const tax = 0.23;
+
+const taxedPrice = prices.map((price, idx, arr) => {
+  const priceObj = {
+    index: idx,
+    taxedPrice: price * tax,
+  };
+  return priceObj;
+});
+
+console.log(prices, taxedPrice);
