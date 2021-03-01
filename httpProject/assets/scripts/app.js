d04d659209b6e1692eec9483f75046bc2e7b25e0
Bezoyan <zoya1905bezoyan@gmail.com>
2021-03-01 21:53:28 +0400

    Get and render Data

--- httpProject/assets/scripts/app.js
+++ httpProject/assets/scripts/app.js
@@ -1,7 +1,20 @@
 const listElement = document.querySelector(".posts");
+const postTempalte = document.getElementById("single-post");
 
 const httpRq = new XMLHttpRequest();
 
 httpRq.open("GET", "https://jsonplaceholder.typicode.com/posts");
 
+httpRq.responseType = "json";
+
+httpRq.onload = function () {
+  //const postList = JSON.parse(httpRq.response);
+  const postList = httpRq.response;
+  for (const post of postList) {
+    const postEl = document.importNode(postTempalte.content, true);
+    postEl.querySelector("h2").textContent = post.title.toUpperCase();
+    postEl.querySelector("p").textContent = post.body;
+    listElement.append(postEl);
+  }
+};
 httpRq.send();
